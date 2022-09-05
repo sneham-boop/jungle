9643763026d8b6e646d6ebdbbfe43e294dd4fd7d
Sneha Mahajan <sneh.km@gmail.com>
2022-09-05 10:35:45 -0400

    Generate new controller for user.

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
   
   get "about", to: "about#index"
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
