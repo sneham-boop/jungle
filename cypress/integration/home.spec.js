d010043659d3d68c4002ff2cc8195db656f31081
Sneha Mahajan <sneh.km@gmail.com>
2022-09-15 18:00:18 -0400

    Add Cypress tests for checking products on home page.

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('Jungle App', () => {
+  it('is able to visit the page', () => {
+    cy.visit('/')
+  });
+  it("There is products on the page", () => {
+    cy.visit('/')
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
